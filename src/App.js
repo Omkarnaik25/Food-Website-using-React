d81fd9c87fb927413faeaa5b7db00da4a7df174a
Omkarnaik25 <113276750+Omkarnaik25@users.noreply.github.com>
2023-10-11 11:35:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Menu from "./Menu";
+import Pagenotfound from "./Pagenotfound";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="*" element={<Pagenotfound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
